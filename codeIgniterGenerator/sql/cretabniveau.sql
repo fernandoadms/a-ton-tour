c5c7d127f8705d278d30abad8441bda51ed60d19
julien.coron <julien.coron@gmail.com>
2011-08-27 07:34:21 +0000

    Installation de l'application

---
+++ codeIgniterGenerator/sql/cretabniveau.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `gaaya`.`gyaniv` (
+	`nividniv` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identifiant', 
+	`nivlbnom` varchar(50) NOT NULL COMMENT 'Nom du niveau des adhérents et des activités'
+);
---
+++ codeIgniterGenerator/sql/cretabtrancheage.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `gaaya`.`gyatra` (
+	`traidtra` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identifiant', 
+	`tralbnom` varchar(50) NOT NULL COMMENT 'Libellé de la tranche d\'age théorique des adhérents'
+);

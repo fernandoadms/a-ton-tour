4d6bf0e3c1c8b4007db59129e0388afaeda98332
julien.coron <julien.coron@gmail.com>
2011-11-20 16:50:16 +0000

    Nettoyage

--- codeIgniterGenerator/sql/cretabprofesseur.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `gaaya`.`gyapro` (
-	`proidpro` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identifiant', 
-	`prolbnom` varchar(255) NOT NULL COMMENT 'Nom du professeur', 
-	`prolbprn` varchar(255) NOT NULL COMMENT 'Prénom du professeur', 
-	`prolbtel` varchar(50) COMMENT 'Téléphone fixe et/ou mobile', 
-	`prolbmai` varchar(255) COMMENT 'Adresse e-mail du professeur', 
-	`protxcmp` varchar(4000) COMMENT 'Compétences techniques, résultats, diplomes, etc. (mini CV)'
-);

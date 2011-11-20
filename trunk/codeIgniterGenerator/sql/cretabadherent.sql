1be9216c239b05594aed7fc5f8e32aad2c652672
julien.coron@gmail.com <julien.coron@gmail.com@caea13e1-16a3-5ac9-7fed-d23231000d09>
2011-11-20 16:50:16 +0000

    Nettoyage

--- trunk/codeIgniterGenerator/sql/cretabadherent.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `gaaya`.`gyaadh` (
-	`adhidadh` int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identifiant', 
-	`adhlbnom` varchar(255) NOT NULL COMMENT 'Nom de l\'adhérent', 
-	`adhlbprn` varchar(255) NOT NULL COMMENT 'Prénom de l\'adhérent', 
-	`adhtxadr` varchar(4000) NOT NULL COMMENT 'Adresse complète, avec code postal et ville', 
-	`adhlbtel` varchar(50) COMMENT 'Téléphone fixe et/ou mobile', 
-	`adhlbmai` varchar(255) COMMENT 'Adresse e-mail de l\'adhérent', 
-	`adhblpho` blob COMMENT 'Photo de l\'adhérent'
-);
--- trunk/codeIgniterGenerator/sql/cretablienactivitesadherents.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `gaaya`.`gyaxaa` (
-	`xaaidact` int NOT NULL COMMENT 'identifiant de l\'activité', 
-	`xaaidadh` int NOT NULL COMMENT 'identifiant de l\'adhérent'
-);

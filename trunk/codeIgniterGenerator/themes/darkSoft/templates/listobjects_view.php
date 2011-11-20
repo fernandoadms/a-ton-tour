9c60887da3d8e344a73781cb53f65bf16b02dba4
julien.coron@gmail.com <julien.coron@gmail.com@caea13e1-16a3-5ac9-7fed-d23231000d09>
2011-11-20 16:48:19 +0000

    Corrections

--- trunk/codeIgniterGenerator/themes/darkSoft/templates/listobjects_view.php
+++ trunk/codeIgniterGenerator/themes/darkSoft/templates/listobjects_view.php
@@ -100,78 +100,15 @@ $(function() {
 });
 </script> 
 
-
-<!--  date picker script -->
-<link rel="stylesheet" href="<?=base_url()?>www/css/datePicker.css" type="text/css" />
-<script src="<?=base_url()?>www/js/jquery/date.js" type="text/javascript"></script>
-<script src="<?=base_url()?>www/js/jquery/jquery.datePicker.js" type="text/javascript"></script>
-<script type="text/javascript" charset="utf-8">
-        $(function()
-{
-
-// initialise the "Select date" link
-$('#date-pick')
-	.datePicker(
-		// associate the link with a date picker
-		{
-			createButton:false,
-			startDate:'01/01/2005',
-			endDate:'31/12/2020'
-		}
-	).bind(
-		// when the link is clicked display the date picker
-		'click',
-		function()
-		{
-			updateSelects($(this).dpGetSelected()[0]);
-			$(this).dpDisplay();
-			return false;
-		}
-	).bind(
-		// when a date is selected update the SELECTs
-		'dateSelected',
-		function(e, selectedDate, $td, state)
-		{
-			updateSelects(selectedDate);
-		}
-	).bind(
-		'dpClosed',
-		function(e, selected)
-		{
-			updateSelects(selected[0]);
-		}
-	);
-	
-var updateSelects = function (selectedDate)
-{
-	var selectedDate = new Date(selectedDate);
-	$('#d option[value=' + selectedDate.getDate() + ']').attr('selected', 'selected');
-	$('#m option[value=' + (selectedDate.getMonth()+1) + ']').attr('selected', 'selected');
-	$('#y option[value=' + (selectedDate.getFullYear()) + ']').attr('selected', 'selected');
-}
-// listen for when the selects are changed and update the picker
-$('#d, #m, #y')
-	.bind(
-		'change',
-		function()
-		{
-			var d = new Date(
-						$('#y').val(),
-						$('#m').val()-1,
-						$('#d').val()
-					);
-			$('#date-pick').dpSetSelected(d.asString());
-		}
-	);
-
-// default the position of the selects to today
-var today = new Date();
-updateSelects(today.getTime());
-
-// and update the datePicker to reflect it...
-$('#d').trigger('change');
-});
-</script>
+<!-- CALENDAR -->
+<script src="<?=base_url()?>www/js/JSCal2-1.8/src/js/jscal2.js"></script>
+<script src="<?=base_url()?>www/js/JSCal2-1.8/src/js/lang/fr.js"></script>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>www/js/JSCal2-1.8/src/css/jscal2.css" />
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>www/js/JSCal2-1.8/src/css/border-radius.css" />
+<style>
+.DynarchCalendar-title { padding-left:40px; }
+</style>
+<!-- /CALENDAR -->
 
 <!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
 <script src="<?=base_url()?>www/js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>

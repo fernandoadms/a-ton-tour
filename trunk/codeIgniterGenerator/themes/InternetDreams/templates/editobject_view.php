a45a664767b9649280cebf5cee3cb71fbe540374
julien.coron@gmail.com <julien.coron@gmail.com@caea13e1-16a3-5ac9-7fed-d23231000d09>
2012-02-22 08:18:14 +0000

    Correctifs

---
+++ trunk/codeIgniterGenerator/themes/InternetDreams/templates/editobject_view.php
@@ -0,0 +1,112 @@
+%[kind : views]
+%[file : edit%%(self.obName.lower())%%_view.php]
+%[path : views]
+<?php
+/*
+ * Created by generator
+ *
+ */
+
+$this->load->helper('form');
+$this->load->helper('url');
+$this->load->helper('template');
+$this->load->helper('views');
+?>
+
+<html>
+<head>
+<? echo htmlHeader('Editer un %%(self.obName)%%'); ?>
+</head>
+<body>
+
+<!-- Start: page-top-outer -->
+<div id="page-top-outer">    
+
+<!-- Start: page-top -->
+<div id="page-top">
+
+	<!-- start logo -->
+	<div id="logo">
+	<a href=""><img src="<?=base_url()?>www/images/shared/logo.png" width="156" height="40" alt="" /></a>
+	</div>
+	<!-- end logo -->
+	
+	<?= htmlSearch() ?>
+	
+ 	<div class="clear"></div>
+
+</div>
+<!-- End: page-top -->
+
+</div>
+<!-- End: page-top-outer -->
+	
+<div class="clear">&nbsp;</div>
+ 
+<!--  start nav-outer-repeat................................................................................................. START -->
+<div class="nav-outer-repeat"> 
+<!--  start nav-outer -->
+<div class="nav-outer"> 
+
+	<?= htmlMyAccount() ?>
+	<?= htmlNavigation("itemA","edit"); ?>
+
+</div>
+<div class="clear"></div>
+<!--  start nav-outer -->
+</div>
+<!--  start nav-outer-repeat................................................... END -->
+
+ <div class="clear"></div>
+ 
+<!-- start content-outer ........................................................................................................................START -->
+<div id="content-outer">
+<!-- start content -->
+<div id="content">
+
+	<!--  start page-heading -->
+	<div id="page-heading">
+		<h1>Editer un %%(self.obName)%%</h1>
+	</div>
+	<!-- end page-heading -->
+	
+	<table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
+	<tr>
+		<th rowspan="3" class="sized"><img src="<?=base_url()?>www/images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+		<th class="topleft"></th>
+		<td id="tbl-border-top">&nbsp;</td>
+		<th class="topright"></th>
+		<th rowspan="3" class="sized"><img src="<?=base_url()?>www/images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+	</tr>
+	<tr>
+		<td id="tbl-border-left"></td>
+		<td>
+		<!--  start content-table-inner ...................................................................... START -->
+		<div id="content-table-inner">
+
+<?php $this->load->view('subviews/%%(self.obName.lower())%%_update_view.php'); ?>
+
+		</div>
+		<!--  end content-table-inner ............................................END  -->
+		</td>
+		<td id="tbl-border-right"></td>
+	</tr>
+	<tr>
+		<th class="sized bottomleft"></th>
+		<td id="tbl-border-bottom">&nbsp;</td>
+		<th class="sized bottomright"></th>
+	</tr>
+	</table>
+	<div class="clear">&nbsp;</div>
+
+</div>
+<!--  end content -->
+<div class="clear">&nbsp;</div>
+</div>
+<!--  end content-outer........................................................END -->
+
+<div class="clear">&nbsp;</div>
+
+
+</body>
+</html>
---
+++ trunk/codeIgniterGenerator/themes/InternetDreams/templates/listobjects.php
@@ -0,0 +1,125 @@
+%[kind : controllers]
+%[file : list%%(self.obName.lower())%%s.php] 
+%[path : controllers]
+<?php
+/*
+ * Created by generator
+ *
+ */
+
+class List%%(self.obName)%%s extends CI_Controller {
+
+	/**
+	 * Constructeur
+	 */
+	function __construct(){
+		parent::__construct();
+		$this->load->model('%%(self.obName)%%_model');
+		$this->load->library('session');
+		$this->load->library('pagination');
+		$this->load->helper('url');
+		$this->load->database();
+%%allAttributeCode = ""
+# inclure les modeles des objets référencés
+
+for field in self.fields:
+	attributeCode = ""
+	if field.referencedObject:
+		attributeCode += """
+		$this->load->model('%s_model');""" % field.referencedObject.obName
+	allAttributeCode += attributeCode
+	
+RETURN = allAttributeCode
+%%
+
+		$this->session->keep_flashdata('msg_info');
+		$this->session->keep_flashdata('msg_confirm');
+		$this->session->keep_flashdata('msg_warn');
+		$this->session->keep_flashdata('msg_error');
+
+	}
+
+	/**
+	 * Affichage des %%(self.obName)%%s
+	 */
+	public function index($orderBy = null, $asc = null, $offset = 0){
+		// preparer le tri
+		if($orderBy == null) {
+			$orderBy = '%%(self.keyFields[0].dbName)%%';
+		}
+		if($asc == null) {
+			$asc = 'asc';
+		}
+		$data['orderBy'] = $orderBy;
+		$data['asc'] = $asc;
+		
+		// preparer la pagination
+		$config['base_url'] = base_url().'index.php/list%%(self.obName.lower())%%s/index/'.$orderBy.'/'.$asc.'/';
+		$config['total_rows'] = %%(self.obName)%%_model::getCount%%(self.obName)%%s($this->db);
+		$config['per_page'] = 15;
+		$config['cur_tag_open'] = '<td class="currentPage">';
+		$config['cur_tag_close'] = '</td>';
+		$config['num_tag_open'] = '<td>';
+		$config['num_tag_close'] = '</td>';
+		$config['prev_tag_open'] = '<td>';
+		$config['prev_tag_close'] = '</td>';
+		$config['next_tag_open'] = '<td>';
+		$config['next_tag_close'] = '</td>';
+		$config['uri_segment'] = '5'; // where the page number is in the URI segment 
+		$this->pagination->initialize($config);
+		$data['pagination'] = $this->pagination;
+		
+		// recuperation des donnees
+		$data['%%(self.obName.lower())%%s'] = %%(self.obName)%%_model::getAll%%(self.obName)%%s($this->db, $orderBy, $asc, $config['per_page'], $offset);
+		%%allAttributeCode = ""
+# inclure les objets référencés dans l'objet $data
+
+for field in self.fields:
+	attributeCode = ""
+	if field.referencedObject:
+		attributeCode += """
+		$data['%(referencedObjectLower)sCollection'] = %(referencedObject)s_model::getAll%(referencedObject)ss($this->db);""" % {
+			'referencedObjectLower' : field.referencedObject.obName.lower(),
+			'referencedObject' : field.referencedObject.obName
+		}
+	allAttributeCode += attributeCode
+	
+RETURN = allAttributeCode
+%%
+		
+		$this->load->view('list%%(self.obName.lower())%%s_view', $data);
+	}
+
+	/**
+	 * Ajout d'un %%(self.obName)%%
+	 */
+	public function add(){
+
+		// Insertion en base
+		$model = new %%(self.obName)%%_model();
+		%%
+includesKey = True;
+RETURN = self.dbAndObVariablesList("$model->(dbVar)s = $this->input->post('(dbVar)s'); ", 'dbVar', 'obVar', 2, includesKey)
+%%
+		$model->save($this->db);
+
+		$this->session->set_flashdata('msg_info', 'Nouveau %%(self.obName)%% ajoute');
+		
+		// Recharge la page avec les nouvelles infos
+		redirect('list%%(self.obName.lower())%%s/index'); 
+	}
+
+	/**
+	 * Suppression d'un %%(self.obName)%%
+	 * @param $%%(self.keyFields[0].dbName)%% identifiant a supprimer
+	 */
+	function delete($%%(self.keyFields[0].dbName)%%){
+		%%(self.obName)%%_model::delete($this->db, $%%(self.keyFields[0].dbName)%%);
+
+		$this->session->set_flashdata('msg_confirm', '%%(self.obName)%% supprime');
+
+		redirect('list%%(self.obName.lower())%%s/index'); 
+	}
+
+}
+?>

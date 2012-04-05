3bf88075a6b68511d9bfe1ceaac92344cb0309b5
julien.coron@gmail.com <julien.coron@gmail.com@caea13e1-16a3-5ac9-7fed-d23231000d09>
2012-04-05 17:11:56 +0000

    Correction du type FILE

--- trunk/codeIgniterGenerator/themes/InternetDreams/templates/listobjects.php
+++ trunk/codeIgniterGenerator/themes/InternetDreams/templates/listobjects.php
@@ -105,11 +105,38 @@ RETURN = self.dbAndObVariablesList("$model->(dbVar)s = $this->input->post('(dbVa
 %%
 		$model->save($this->db);
 %%codeForUploadFile = ""
+useUpload = False
 for field in self.fields:
 	attributeCode = ""
 	if field.sqlType.upper() == "FILE":
+		useUpload = True
 		attributeCode += """
 		// Upload du fichier %(dbName)s : %(desc)s
+		$uploadDataFile_%(dbName)s = null;
+		if( $this->input->post('%(dbName)s_file') != "" && $this->upload->do_upload('%(dbName)s_file')){
+			$uploadDataFile_%(dbName)s = $this->upload->data('%(dbName)s_file');
+		} else {
+			// erreur
+			if( $this->input->post('%(dbName)s_file') != "" ){
+				$this->session->set_flashdata('msg_error', $this->upload->display_errors());
+			}
+		}
+		if($uploadDataFile_%(dbName)s != null) {
+			$model->%(dbName)s = '%(obName)s_%(dbName)s_' . $model->%(keyField)s . '_file' . $uploadDataFile_%(dbName)s['file_ext'];
+			rename($path . $uploadDataFile_%(dbName)s['file_name'], $path . $model->%(dbName)s);
+			// suppression de l'image téléchargée
+			unlink($path . $uploadDataFile_%(dbName)s['file_name']);
+			$model->update($this->db);
+		}""" % {'dbName' : field.dbName, 
+			'desc' : field.description, 
+			'obName' : self.obName,
+			'keyField' : self.keyFields[0].dbName
+		}
+	codeForUploadFile += attributeCode
+
+if useUpload:
+	codeForUploadFile = """
+		// Configuration pour chargement des fichiers 
 		$config['upload_path'] = realpath('www/uploads/');
 		$config['allowed_types'] = 'gif|jpg|png|jpeg';
 		$config['max_size']	= '0';
@@ -117,27 +144,8 @@ for field in self.fields:
 		$config['max_height']  = '0';
 		$this->load->library('upload', $config);
 		$path = $config['upload_path'] . "/";
-		$thumbnailsPath = $path . "thumbnails" . "/";
+""" + codeForUploadFile
 		
-		$uploadDataImage = null;
-		if($this->upload->do_upload('%(dbName)s_file')){
-			$uploadDataImage = $this->upload->data('%(dbName)s_file');
-		} else {
-			// pas de fichier à charger
-		}
-		if($uploadDataImage != null) {
-			$model->%(dbName)s = '%(obName)s_%(dbName)s_' . $model->%(keyField)s . '_file' . $uploadDataImage['file_ext'];
-			rename($path . $uploadDataImage['file_name'], $path . $model->%(dbName)s);
-			// suppression de l'image téléchargée
-			unlink($path . $uploadDataImage['file_name']);
-			$model->save($this->db);
-		}""" % {'dbName' : field.dbName, 
-			'desc' : field.description, 
-			'obName' : self.obName
-			'keyField' : self.keyFields[0].dbName
-		}
-	codeForUploadFile += attributeCode
-
 RETURN = codeForUploadFile
 %%
 

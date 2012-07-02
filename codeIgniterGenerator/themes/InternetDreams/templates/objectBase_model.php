630607d19bb517a4a3c8d8ea7d077fa789f8522b
julien.coron <julien.coron@gmail.com>
2012-07-02 19:21:23 +0000

    Type ENUM donne un tableau avec les options

--- codeIgniterGenerator/themes/InternetDreams/templates/objectBase_model.php
+++ codeIgniterGenerator/themes/InternetDreams/templates/objectBase_model.php
@@ -21,6 +21,25 @@ RETURN = self.dbVariablesList("""/**
 \tvar $(instVar)s;
 """, 'instVar',  'typeVar', 'descrVar', 1)
 %%
+
+	%%
+allAttributesCode = ""
+for field in self.fields:
+	attributeCode = ""
+	if field.sqlType.upper()[0:4] == "ENUM":
+		attributeCode = "static $liste_%(dbName)s = array(" % { 'dbName' : field.dbName }
+		enumTypes = field.sqlType[5:-1]
+		typeList = ""
+		for enum in enumTypes.split(','):
+			valueAndText = enum.replace('"','').replace("'","").split(':')
+			typeList += """"%(value)s"=>"%(text)s",""" % {'value': valueAndText[0].strip(), 'text': valueAndText[1].strip()}
+		typeList = typeList[:-1]
+		attributeCode += typeList + ");"
+	if allAttributesCode != "":
+		allAttributesCode += "\n\t"
+	allAttributesCode += attributeCode
+RETURN = allAttributesCode
+%%
 	
 	/**
 	 * Constructeur

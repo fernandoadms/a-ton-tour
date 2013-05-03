347429eb83d2e1c0e363b70d2b8c8864da950f17
julien.coron <julien.coron@gmail.com>
2013-05-03 19:58:06 +0000

    Template pour générer les fichiers de langue

---
+++ codeIgniterGenerator/themes/bootstrap/templates/messages_lang_fr.php
@@ -0,0 +1,29 @@
+%[kind : lang]
+%[file : messages_%%(self.obName.lower())%%s_lang.php] 
+%[path : language/french]
+<?php
+$lang['%%(self.obName.lower())%%.message.confirm.deleted'] = "%%(self.obName.lower())%% supprimé";
+$lang['%%(self.obName.lower())%%.message.confirm.added'] = "%%(self.obName.lower())%% créé avec succès";
+$lang['%%(self.obName.lower())%%.message.confirm.modified'] = "%%(self.obName.lower())%% mis à jour avec succès";
+
+$lang['%%(self.obName.lower())%%.form.create.title'] = "Ajouter un %%(self.displayName)%%";
+$lang['%%(self.obName.lower())%%.form.edit.title'] = "Editer un %%(self.displayName)%%";
+$lang['%%(self.obName.lower())%%.form.list.title'] = "Liste des %%(self.displayName)%%s";
+
+
+
+%%allAttributesCode = ""
+for field in self.fields:
+	attributeCode = """$lang['%(objectObName)s.form.%(dbName)s.label'] = "%(obName)s";
+$lang['%(objectObName)s.form.%(dbName)s.description'] = "%(desc)s";  
+""" % {	'dbName': field.dbName, 
+		'obName': field.obName,
+		'objectObName':self.obName.lower(),
+		'desc' : field.description
+	}
+	allAttributesCode += attributeCode
+	
+RETURN = allAttributesCode
+%%
+
+?>
